b33dc896e91568edd3ad29328dee931ebfee9af3
Alec Mitchell <alecpm@gmail.com>
2014-04-13 13:31:35 -0400

    Add a name to metadata
    Required by Berkshelf

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ license 'All rights reserved'
 description 'Installs and configures glusterfs as a server or client.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version '0.1.1'
+name 'glusterfs'
 recipe 'glusterfs', 'Installs glusterfs::server and glusterfs::client.'
 recipe 'glusterfs::server', 'Installs and configures glusterfs server.'
 recipe 'glusterfs::peer', 'Installs and configures glusterfs server as a peer.'
